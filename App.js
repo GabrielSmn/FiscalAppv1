3c520f8f60ebcae78b3acd11c2d3ed9a27c62e1d
GabrielSmn <gabriel.simeone@gmail.com>
2023-07-07 17:12:24 -0300

    feat(basic) Tira foto e salva

--- App.js
+++ App.js
@@ -1,85 +1,126 @@
-import { Modal, SafeAreaView, StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native';
-import { Camera } from 'expo-camera'
-import React, { useState,useEffect, useRef } from 'react';
-import { FontAwesome } from '@expo/vector-icons'
+import React, { useState, useEffect } from 'react';
+import { View, Text, Image, TouchableOpacity, Modal, Alert } from 'react-native';
+import { Camera } from 'expo-camera';
+import { Ionicons } from '@expo/vector-icons';
+import * as SQLite from 'expo-sqlite';
 
 export default function App() {
-  const camRef = useRef(null);
-  const [type,setType]=useState(Camera.Constants.Type.back)
-  const [hasPermission, setHaspermission ]=useState(null);
-  const [capturedPhoto, setCapturedPhoto] = useState(null);
-  const [open,setOpen]= useState(false);
+  const db = SQLite.openDatabase('photos.db');
 
-  useEffect(()=>{
-    (async()=> {
-      const {status}= await Camera.requestCameraPermissionsAsync();
-      setHaspermission(status==='granted')
-    })();
-  },[]);
-  if(hasPermission===null){return <View/>}
-  if(hasPermission==='false'){return <Text>Acesso negado!</Text>}
-
-  async function takePicture(){
-    if(camRef){
-      const data = await camRef.current.takePictureAsync();
-      console.log(data)
-      setOpen(true)
-      setCapturedPhoto(data.uri)
-    } 
-
-  }
-
-  return (
-    <SafeAreaView style={styles.container}>
-      <Camera 
-      style={{flex:1}}
-      type={type}
-      ref={camRef}
-      >
-      </Camera>
-     <TouchableOpacity style={styles.button} onPress={takePicture}>
-    <FontAwesome name='camera' size={23} color ="#FF0000"></FontAwesome>
-     </TouchableOpacity>
-
-
-     {capturedPhoto &&
-
-     <Modal
-     animationType='slide'
-     transparent={false}
-     visible={open}
-     >
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', margin: '20' }}>
-        <TouchableOpacity style={{margin:10}} onPress={()=>setOpen(false)}>
-          <FontAwesome name='window-close' size={50}color='#121212'/>
-
-        </TouchableOpacity>
-
-        <Image
-        style={{width:'100%',height:300,borderRadius:20}}
-        source={{uri:capturedPhoto}}
+    const [hasPermission, setHasPermission] = useState(null);
+    const [camera, setCamera] = useState(null);
+    const [imageUri, setImageUri] = useState(null);
+    const [showModal, setShowModal] = useState(false);
+    const [isSaving, setIsSaving] = useState(false);
+  
+    useEffect(() => {
+      (async () => {
+        const { status } = await Camera.requestCameraPermissionsAsync();
+        setHasPermission(status === 'granted');
+      })();
+    }, []);
+  
+    useEffect(() => {
+      db.transaction((tx) => {
+        tx.executeSql(
+          'CREATE TABLE IF NOT EXISTS photos (id INTEGER PRIMARY KEY AUTOINCREMENT, uri TEXT);'
+        );
+      });
+    }, []);
+  
+    const takePicture = async () => {
+      if (camera) {
+        const photo = await camera.takePictureAsync();
+  
+        setImageUri(photo.uri);
+        setShowModal(true);
+      }
+    };
+  
+    const savePhoto = () => {
+      db.transaction((tx) => {
+        tx.executeSql('INSERT INTO photos (uri) VALUES (?);', [imageUri], (_, { rowsAffected }) => {
+          if (rowsAffected > 0) {
+            Alert.alert('Foto Salva', `A foto foi salva com sucesso.\n\nURI: ${imageUri}`);
+            setIsSaving(false);
+            closeModal()
+          }
+        });
+      });
+    };
+  
+    const closeModal = () => {
+      setShowModal(false);
+      setImageUri(null);
+    };
+  
+    return (
+      <View style={{ flex: 1 }}>
+        <Camera
+          style={{ flex: 1 }}
+          type={Camera.Constants.Type.back}
+          ref={(ref) => setCamera(ref)}
         />
-          
+  
+        {imageUri ? (
+          <View style={styles.captureButtonsContainer}>
+            <TouchableOpacity style={styles.captureButton} onPress={takePicture}>
+              <Ionicons name="camera" size={24} color="white" />
+            </TouchableOpacity>
+          </View>
+        ) : (
+          <TouchableOpacity style={styles.captureButton} onPress={takePicture}>
+            <Ionicons name="camera" size={24} color="white" />
+          </TouchableOpacity>
+        )}
+  
+        <Modal visible={showModal} onRequestClose={closeModal}>
+          <View style={{ flex: 1, backgroundColor: 'black' }}>
+            {imageUri && (
+              <Image source={{ uri: imageUri }} style={styles.modalImage} resizeMode="contain" />
+            )}
+            <TouchableOpacity style={styles.closeButton} onPress={closeModal}>
+              <Ionicons name="close-circle" size={32} color="white" />
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.saveButton} onPress={savePhoto}>
+              <Ionicons name="save" size={24} color="white" />
+            </TouchableOpacity>
+          </View>
+        </Modal>
       </View>
-      
-      </Modal>}
-      
- 
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-  },
-  button:{
-    justifyContent:'center',
-    alignItems:'center',
-    backgroundColor:'#121212',
-    margin: 20,
-    borderRadius: 10,
-    height: 50,
-  }
-});
+    );
+  };
+  
+  const styles = {
+    captureButton: {
+      position: 'absolute',
+      bottom: 20,
+      alignSelf: 'center',
+      backgroundColor: 'rgba(0, 0, 0, 0.7)',
+      borderRadius: 50,
+      padding: 16,
+    },
+    saveButton: {
+      position: 'absolute',
+      bottom: 20,
+      right: 20,
+      backgroundColor: 'rgba(0, 0, 0, 0.7)',
+      borderRadius: 50,
+      padding: 16,
+    },
+    modalImage: {
+      flex: 1,
+    },
+    closeButton: {
+      position: 'absolute',
+      top: 40,
+      right: 20,
+    },
+    captureButtonsContainer: {
+      position: 'absolute',
+      bottom: 20,
+      alignSelf: 'center',
+      flexDirection: 'row',
+    },
+  };
+  
